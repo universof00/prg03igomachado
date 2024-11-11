86ad4c80826c506db6f086e9c60bd6c6b784c589
Igo Machado <alphatestesv1@gmail.com>
2024-11-11 20:22:28 -0300

    ADD: Atividade 7

---
+++ src/main/java/br/com/ifba/atividade07/view/Fatorar.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package br.com.ifba.atividade07.view;
+
+public class Fatorar extends javax.swing.JFrame {
+
+    public Fatorar() {
+        initComponents();
+    }
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblvalor = new javax.swing.JLabel();
+        spnvalor = new javax.swing.JSpinner();
+        btncalcular = new javax.swing.JButton();
+        lblformula = new javax.swing.JLabel();
+        lblresultado = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblvalor.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblvalor.setText("Valor:");
+
+        spnvalor.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        spnvalor.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
+
+        btncalcular.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btncalcular.setText("Calcular");
+        btncalcular.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btncalcularActionPerformed(evt);
+            }
+        });
+
+        lblformula.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblformula.setText("Resultado:");
+
+        lblresultado.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblresultado.setText("0");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblformula, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(lblresultado, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblvalor, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(spnvalor, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btncalcular, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(139, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(46, 46, 46)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btncalcular, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(lblvalor)
+                        .addComponent(spnvalor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(71, 71, 71)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lblformula, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblresultado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(136, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btncalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncalcularActionPerformed
+        Fatorial fatorial = new Fatorial();
+        int numeoro = Integer.parseInt(spnvalor.getValue().toString());
+        fatorial.setValor(numeoro);
+        lblformula.setText(fatorial.getFormula());
+        lblresultado.setText(Integer.toString(fatorial.getFatorial()));  
+    }//GEN-LAST:event_btncalcularActionPerformed
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Fatorar().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btncalcular;
+    private javax.swing.JLabel lblformula;
+    private javax.swing.JLabel lblresultado;
+    private javax.swing.JLabel lblvalor;
+    private javax.swing.JSpinner spnvalor;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/main/java/br/com/ifba/atividade07/view/Fatorial.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade07.view;
+
+public class Fatorial {
+     private int n;
+     private int fat = 1;
+     private String formula = "";
+
+    public void setValor(int n) {
+        int num = n;
+        int f = 1;
+        String s = "";
+        for(int i = n; i > 1; i--){
+            f*= i;
+            s += i + "*"; 
+        }
+        s += "1 = ";
+        fat = f;
+        formula = s;
+    }
+
+    public int getFatorial() {
+         return fat;
+    }
+
+    public String getFormula() {
+        return formula;
+    }
+}
